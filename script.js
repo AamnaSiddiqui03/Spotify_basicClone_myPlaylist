da2be30218a64437807c7733ae108b0bd259cf24
AamnaSiddiqui03 <116269502+AamnaSiddiqui03@users.noreply.github.com>
2023-09-24 23:32:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,17 +10,17 @@ let masterSongName = document.getElementById('masterSongName');
 let songItems = Array.from(document.getElementsByClassName('songItem'));
 
 let songs = [
-    {songName: "Saudebaazi - Aakarosh", filePath: "songs/1.mp3", coverPath: "covers/1.jpg" , duration: "05:55"},
-    {songName: "Chahun main ya na - Arjit singh", filePath: "songs/2.mp3", coverPath: "covers/2.jpg" , duration: "04:01"},
-    {songName: "Saranga Dariya - Sai pallavi", filePath: "songs/3.mp3", coverPath: "covers/3.jpg", duration: "04:00"},
+    {songName: "Saudebaazi-Aakarosh", filePath: "songs/1.mp3", coverPath: "covers/1.jpg" , duration: "05:55"},
+    {songName: "Chahun main ya na", filePath: "songs/2.mp3", coverPath: "covers/2.jpg" , duration: "04:01"},
+    {songName: "Saranga Dariya", filePath: "songs/3.mp3", coverPath: "covers/3.jpg", duration: "04:00"},
     {songName: "Roke na ruke naina", filePath: "songs/4.mp3", coverPath: "covers/4.jpg" , duration: "04:39"},
     {songName: "Daaru Badnaam", filePath: "songs/5.mp3", coverPath: "covers/5.jpg", duration: "04:01"},
-    {songName: "Ye Tune Kya Kiya [Slowed-Reverb]", filePath: "songs/6.mp3", coverPath: "covers/6.jpg", duration: "06:23"},
+    {songName: "Ye Tune Kya Kiya-Slowed", filePath: "songs/6.mp3", coverPath: "covers/6.jpg", duration: "06:23"},
     {songName: "Pretty Woman", filePath: "songs/7.mp3", coverPath: "covers/7.jpg", duration: "05:30"},
-    {songName: "Heeriye- Jasleen Royal", filePath: "songs/8.mp3", coverPath: "covers/8.jpg" , duration: "03:19"},
+    {songName: "Heeriye-Jasleen Royal", filePath: "songs/8.mp3", coverPath: "covers/8.jpg" , duration: "03:19"},
     {songName: "Behti hawa se tha woh", filePath: "songs/9.mp3", coverPath: "covers/9.jpg" , duration: "05:14"},
-    {songName: "Mere Humsafar - ost", filePath: "songs/10.mp3", coverPath: "covers/10.jpg", duration: "05:52"},
-    {songName: "Baarishein - Anuv Jain", filePath: "songs/11.mp3", coverPath: "covers/11.jpg",duration: "03:28"}
+    {songName: "Mere Humsafar-ost", filePath: "songs/10.mp3", coverPath: "covers/10.jpg", duration: "05:52"},
+    {songName: "Baarishein-Anuv Jain", filePath: "songs/11.mp3", coverPath: "covers/11.jpg",duration: "03:28"}
 ]
 
 songItems.forEach((element, i)=>{ 
